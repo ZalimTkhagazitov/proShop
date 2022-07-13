74d90d6c6bf786034d4fde49768c3e0e5ee84078
Zalim Tkhagazitov <bbulletb@gmail.com>
2022-07-13 01:53:18 -0400

    proShop

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import { Container, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
